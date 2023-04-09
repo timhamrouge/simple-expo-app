4a7375bcd971c5a151e5ddfb8f038d2f9184c9c3
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-04-09 20:35:56 +0100

    starting todos

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const onAddTodo = () => {
+    setTodos([...todos, {name: 'new todo']})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Todo list</Text>
+      <TextInput
+        value="hello world"
+      />
+      <Button
+        onPress={onAddTodo}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      {todos.map((todo) => {
+        return (
+          <div>
+            {todo}
+          </div>
+        )
+      })}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
